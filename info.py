2bebf0f0d9a82712b9230c102a9db7f0d03ef04d
vipsrmbot <vipsrmbot@gmail.com>
2025-07-12 19:21:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -130,16 +130,16 @@ SEASONS = [f"season {i}" for i in range(1, 23)]
 START_IMG = (
     environ.get(
         "START_IMG",
-        "https://i.ibb.co/qpxpGmC/image.jpg https://i.ibb.co/DQ35zLZ/image.jpg",
+        "https://graph.org/file/9dfe2cffbe645906179ed-f0a79ab1dbb547bb2a.jpg https://graph.org/file/0004fa408a3f69a611605-6b8216dd8ea49e3ac8.jpg https://graph.org/file/1c8ea5661a745f0c4e925-135cfedfa36b6f2ba4.jpg",
     )
 ).split()
 FORCESUB_IMG = environ.get("FORCESUB_IMG", "https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg")
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split()
 PAYPICS = (
-    environ.get("PAYPICS", "https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg")
+    environ.get("PAYPICS", "https://envs.sh/eOk.jpg")
 ).split()
 SUBSCRIPTION = environ.get(
-    "SUBSCRIPTION", "https://graph.org/file/9f3f47c690bbcc67633c2.jpg"
+    "SUBSCRIPTION", "https://envs.sh/eO8.jpg"
 )
 REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 
