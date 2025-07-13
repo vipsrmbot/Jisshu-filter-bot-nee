ab734bfd73358e65a4b65451b3158e9cd8681e00
vipsrmbot <vipsrmbot@gmail.com>
2025-07-13 11:08:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -153,7 +153,7 @@ IS_SEND_MOVIE_UPDATE = is_enabled(
 )  # Don't Change It ( If You Want To Turn It On Then Turn It On By Commands) We Suggest You To Make It Turn Off If You Are Indexing Files First Time.
 MAX_BTN = int(environ.get("MAX_BTN", "8"))
 AUTO_DELETE = is_enabled("AUTO_DELETE", True)
-DELETE_TIME = int(environ.get("DELETE_TIME", 1200))
+DELETE_TIME = int(environ.get("DELETE_TIME", 400))
 IMDB = is_enabled("IMDB", False)
 FILE_CAPTION = environ.get("FILE_CAPTION", f"{script.FILE_CAPTION}")
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
