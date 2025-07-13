b7af90728096d6f2767c536e11acae1e3b6c064f
vipsrmbot <vipsrmbot@gmail.com>
2025-07-13 08:02:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -117,7 +117,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """```<b><a href="https://t.me/VIPSRM">{file_name}```</a></b>\n<b>𝙹𝚘𝚒𝚗☞: <a herf="http://t.me/MRk2xBots">[@MRk2xBots]</a></b>"""
+    FILE_CAPTION = """```{file_name}```</a></b>\n<b>𝙹𝚘𝚒𝚗☞: <a herf="http://t.me/MRk2xBots">[@MRk2xBots]</a></b>"""
 
     ALRT_TXT = """ᴡʜᴀᴛ ᴀʀᴇ ʏᴏᴜ sᴇᴀʀᴄʜɪɴɢ !?"""
 
